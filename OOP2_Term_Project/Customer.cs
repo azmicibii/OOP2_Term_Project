1a0ff31c378b2935414f3d198ac83e8d66eebb31
Azmi Cibi <acibi.07@outlook.com>
2017-05-23 22:45:28 +0300

    Customer class

---
+++ OOP2_Term_Project/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2_Term_Project
+{
+    class Customer
+    {
+        private long id;
+        private string name;
+        private string adress;
+        private string email;
+        private string username;
+        private string password;
+
+        public long Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Adress { get => adress; set => adress = value; }
+        public string Email { get => email; set => email = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
